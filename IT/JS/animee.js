ec659c6280b128bad200bf9dc96982ea7919ab6b
EgorTsymbal <djokeron@bk.ru>
2023-12-05 09:22:18 +0700

    betta

--- IT/JS/animee.js
+++ IT/JS/animee.js
@@ -1,9 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const audio = document.getElementById('backgroundMusic');
     const soundImage = document.getElementById('soundImage');
-    const thirdTitle = document.querySelector('.third__title');
-    const overlay = document.querySelector('.overlay');
-    const overlayVideo = document.getElementById('overlayVideo');
     const animeText = document.querySelector('.anime_text');
 
     let hideOverlayTimeout;
@@ -30,39 +27,6 @@ document.addEventListener('DOMContentLoaded', function () {
                 });
             }
         });
-    });
-
-    thirdTitle.addEventListener('mouseenter', function () {
-        overlay.style.display = 'flex';
-        overlayVideo.play();
-    });
-    
-    thirdTitle.addEventListener('mouseleave', function () {
-        hideOverlayTimeout = setTimeout(function () {
-            overlay.style.display = 'none';
-            overlayVideo.pause();
-        }, 3000);
-    });
-    
-
-    thirdTitle.addEventListener('mousemove', function () {
-        clearTimeout(hideOverlayTimeout);
-    });
-
-    document.addEventListener('mouseleave', function (event) {
-        const cursorArea = 1000;
-
-        const isCursorInTitle = (
-            event.clientX >= thirdTitle.offsetLeft - cursorArea &&
-            event.clientX <= thirdTitle.offsetLeft + thirdTitle.offsetWidth + cursorArea &&
-            event.clientY >= thirdTitle.offsetTop - cursorArea &&
-            event.clientY <= thirdTitle.offsetTop + thirdTitle.offsetHeight + cursorArea
-        );
-
-        if (!isCursorInTitle && !overlay.contains(event.relatedTarget)) {
-            overlay.style.display = 'none';
-            overlayVideo.pause();
-        }
     });
 
     document.querySelector('.icon').addEventListener('click', function () {
